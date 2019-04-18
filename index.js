e41db1fb56980dbb01089714208bd52aafd3a0a9
= <efenstakes101@gmail.com>
2019-04-19 00:08:48 +0300

    add skeleton application structure

---
+++ index.js
@@ -0,0 +1,15 @@
+// import external libraries
+const express = require('express')
+
+
+// import internal libraries
+
+
+// setup passport authentication
+
+
+
+// hook up routes with controllers
+
+
+// start app on
